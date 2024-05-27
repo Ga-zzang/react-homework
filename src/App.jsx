de9c8642337fb562b5df0d575c4548d49cfa57fd
GA HYEON <dlrkxkszz@gmail.com>
2024-05-28 01:08:45 +0900

    1강 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+// 과제 6번
+import { Fragment } from "react";
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  // 과제 2번
+  const addCount = () => {
+    setCount(count + 1);
+  };
+  
+  // 과제 3번
+  const minusCount = () => {
+	  setCount(count - 1);
+  };
+
+  // 과제 4번 
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div style={{
+      // 과제 5번
+      backgroundColor: count ? "red" : "black",
+      color: "white",
+    }}>
+      {/* 과제 1번 */}
+	    <button onClick={minusCount}>-</button>
+      Count: {count}
+      <button onClick={addCount}>+</button>
+      <button onClick={resetCount}>reset</button>
+    </div>
+  );
 }
 
 export default App;
